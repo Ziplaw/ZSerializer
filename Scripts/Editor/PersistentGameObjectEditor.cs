889ff7bf5e9183a4f82cb05369bbcc2bf78c7824
Ziplaw <afrgplus@gmail.com>
2021-08-09 17:11:06 +0200

    Removed residual PersistentGameObject Settings

--- Scripts/Editor/PersistentGameObjectEditor.cs
+++ Scripts/Editor/PersistentGameObjectEditor.cs
@@ -12,23 +12,6 @@ using ZSerializer.Editor;
 public class PersistentGameObjectEditor : Editor
 {
     private static ZSaverStyler styler;
-    private PersistentGameObject manager;
-    private IEnumerable<Type> serializableTypes;
-
-    private void OnEnable()
-    {
-        serializableTypes = ZSave.ComponentSerializableTypes;
-        manager = target as PersistentGameObject;
-        // if (!Application.isPlaying && manager._componentDatas == null || manager._componentDatas.Count == 0)
-        // {
-        //     manager.UpdateSerializableComponents();
-        //     // manager._componentDatas =
-        //     //     (from c in manager.GetComponents<Component>().Where(c =>
-        //     //             ZSave.ComponentSerializableTypes.Contains(c.GetType()) &&
-        //     //             !c.GetType().IsSubclassOf(typeof(MonoBehaviour)))
-        //     //         select new PersistentGameObject.SerializableComponentData(c.GetType())).ToArray();
-        // }
-    }
 
     [DidReloadScripts]
     static void OnDatabaseReload()
@@ -36,50 +19,14 @@ public class PersistentGameObjectEditor : Editor
         styler = new ZSaverStyler();
     }
 
-    private bool showSettings;
 
     public override void OnInspectorGUI()
     {
         serializedObject.Update();
-        // base.OnInspectorGUI();
+        
         using (new EditorGUILayout.HorizontalScope("helpbox"))
         {
             GUILayout.Label("<color=#29cf42>Persistent GameObject</color>", styler.header, GUILayout.MinHeight(32));
-            showSettings = GUILayout.Toggle(showSettings, styler.cogWheel, new GUIStyle("button"),
-                GUILayout.MaxHeight(32), GUILayout.MaxWidth(32));
-            
-
-            // if (manager.GetComponents<Component>().Count(c => !c.GetType().IsSubclassOf(typeof(MonoBehaviour)) && c.GetType() != typeof(Transform)) !=
-            //     manager._componentDatas.Count)
-            // {
-            //     manager.UpdateSerializableComponents(serializableTypes);
-            //     return;
-            // }
-        }
-
-        if (showSettings)
-        {
-            // var managerComponentDatas = manager._componentDatas;
-            // using (new GUILayout.VerticalScope("helpbox"))
-            // {
-            //     using (new GUILayout.VerticalScope("box"))
-            //     {
-            //         GUILayout.Label("Components to Serialize",
-            //             new GUIStyle("label") {alignment = TextAnchor.MiddleCenter});
-            //         // if(GUILayout.Button())
-            //     }
-            //
-            //     for (var i = 0; i < managerComponentDatas.Count; i++)
-            //     {
-            //         using (new GUILayout.HorizontalScope())
-            //         {
-            //             EditorGUILayout.PropertyField(
-            //                 serializedObject.FindProperty(nameof(manager._componentDatas)).GetArrayElementAtIndex(i)
-            //                     .FindPropertyRelative("serialize"), GUIContent.none, GUILayout.MaxWidth(20));
-            //             GUILayout.Label(Type.GetType(managerComponentDatas[i].typeName).ToString().Split('.').Last());
-            //         }
-            //     }
-            // }
         }
 
         serializedObject.ApplyModifiedProperties();
--- Scripts/Runtime/PersistentGameObject.cs
+++ Scripts/Runtime/PersistentGameObject.cs
@@ -8,47 +8,6 @@ using Component = UnityEngine.Component;
 [AddComponentMenu("ZSaver/Persistent GameObject"), DisallowMultipleComponent]
 public class PersistentGameObject : MonoBehaviour
 {
-    [Serializable]
-    public struct SerializableComponentData
-    {
-        public string typeName;
-        public bool serialize;
-
-        public SerializableComponentData(Type type)
-        {
-            typeName = type.FullName + ", " + type.Assembly;
-            serialize = true;
-        }
-    }
-
-    // [NonZSerialized] public List<SerializableComponentData> _componentDatas = new List<SerializableComponentData>();
-    // public void UpdateSerializableComponents(IEnumerable<Type> serializableTypes)
-    // {
-    //     // if (!Application.isPlaying)
-    //     {
-    //         var componentTypes = GetComponents<Component>().Where(c => !c.GetType().IsSubclassOf(typeof(MonoBehaviour)) && c.GetType() != typeof(Transform)).Select(c => c.GetType());
-    //
-    //         var serializableComponentTypes = _componentDatas.Select(c => Type.GetType(c.typeName));
-    //
-    //         for (var i = 0; i < serializableComponentTypes.Count(); i++)
-    //         {
-    //             if (!componentTypes.Contains(serializableComponentTypes.ElementAt(i)) && i < _componentDatas.Count)
-    //             {
-    //                 _componentDatas.RemoveAt(i);
-    //             }
-    //         }
-    //
-    //         for (var i = 0; i < componentTypes.Count(); i++)
-    //         {
-    //             if (!serializableComponentTypes.Contains(componentTypes.ElementAt(i)))
-    //             {
-    //                 _componentDatas.Add(new SerializableComponentData(componentTypes.ElementAt(i)));
-    //             }
-    //         }
-    //     }
-    // }
-    
-
     public static int CountParents(Transform transform)
     {
         int totalParents = 1;
