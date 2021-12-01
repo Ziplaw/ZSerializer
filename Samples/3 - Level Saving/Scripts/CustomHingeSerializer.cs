388783e04dad67b3bbb15e00bef316cb93d81c58
Ziplaw <afrgplus@gmail.com>
2021-12-01 18:56:39 +0100

    Asset preparation

--- Samples/3 - Level Saving/Scripts/CustomHingeSerializer.cs
+++ Samples/3 - Level Saving/Scripts/CustomHingeSerializer.cs
@@ -8,10 +8,10 @@ public class CustomHingeSerializer : MonoBehaviour
     public static void SerializeJoint(ZSerializer.Internal.ZSerializer serializer, Component component)
     {
         Debug.LogWarning("<color=yellow>SerializeJoint has been disabled prior to the building of the Unity Component's ZSerializers. Please uncomment these lines for the serializer to work.</color>");
-        var hinge = (HingeJoint2D)component;
-        var zserializer = (HingeJoint2DZSerializer)serializer;
-        zserializer.serializableLimits = new Vector2(hinge.limits.min, hinge.limits.max);
-        zserializer.serializableMotor = new Vector2(hinge.motor.motorSpeed, hinge.motor.maxMotorTorque);
+        // var hinge = (HingeJoint2D)component;
+        // var zserializer = (HingeJoint2DZSerializer)serializer;
+        // zserializer.serializableLimits = new Vector2(hinge.limits.min, hinge.limits.max);
+        // zserializer.serializableMotor = new Vector2(hinge.motor.motorSpeed, hinge.motor.maxMotorTorque);
     }
 
     public static void DeserializeJoint(ZSerializer.Internal.ZSerializer serializer, Component component)
@@ -19,22 +19,22 @@ public class CustomHingeSerializer : MonoBehaviour
         Debug.LogWarning("<color=yellow>DeserializeJoint has been disabled prior to the building of the Unity Component's ZSerializers. Please uncomment these lines for the serializer to work.</color>");
 
         
-        var hinge = (HingeJoint2D)component;
-        var hingeSerializer = (HingeJoint2DZSerializer)serializer;
-        
-        hinge.limits = new JointAngleLimits2D
-        {
-            min = hingeSerializer.serializableLimits.x,
-            max = hingeSerializer.serializableLimits.y
-        };
-        
-        hinge.motor = new JointMotor2D
-        {
-            motorSpeed = hingeSerializer.serializableMotor.x,
-            maxMotorTorque = hingeSerializer.serializableMotor.y
-        };
-        
-        hinge.useLimits = hingeSerializer.useLimits;
-        hinge.useMotor = hingeSerializer.useMotor;
+        // var hinge = (HingeJoint2D)component;
+        // var hingeSerializer = (HingeJoint2DZSerializer)serializer;
+        //
+        // hinge.limits = new JointAngleLimits2D
+        // {
+        //     min = hingeSerializer.serializableLimits.x,
+        //     max = hingeSerializer.serializableLimits.y
+        // };
+        //
+        // hinge.motor = new JointMotor2D
+        // {
+        //     motorSpeed = hingeSerializer.serializableMotor.x,
+        //     maxMotorTorque = hingeSerializer.serializableMotor.y
+        // };
+        //
+        // hinge.useLimits = hingeSerializer.useLimits;
+        // hinge.useMotor = hingeSerializer.useMotor;
     }
 }
